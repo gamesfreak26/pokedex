e4802f310cd6c43940f9b9815126ee9ee64c85a1
Tharanga Kasthuriarachchi <gamesfreak26@gmail.com>
2021-05-02 17:27:05 +1000

    Get pokemon names

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import axios from 'axios'
+import { useState, useEffect } from 'react';
 
-function App() {
+const uri = "https://pokeapi.co/api/v2/generation/1"
+
+function App() {  
+  const [generation1, setGeneration1] = useState(null)
+  
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  
+  const fetchData = async () => {
+    const response = await axios.get(uri)
+    setGeneration1(response.data)
+    console.log(response.data)
+  }
+
+  const Capitalize = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Pokemon</h1>
+      <p>Choose from the following pokemon:</p>
+      <h3>Generation 1</h3>
+      <div className="pokemon-list">
+        {generation1 && generation1.pokemon_species.map((pokemon, index) => {
+          return <div key={index}>{Capitalize(pokemon.name)}</div>
+        })}
+      </div>
     </div>
   );
 }
